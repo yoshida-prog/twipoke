e523ce34cc5b4b357553531534cfccbbd86cfd90
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-08-31 23:47:20 +0900

    Update followerStatus.js

--- twipoke/config/followerStatus.js
+++ twipoke/config/followerStatus.js
@@ -1,15 +1,15 @@
 export default
 {
-    "hp_max": 2,
-    "hp_min": 1,
+    "hp_max": 200,
+    "hp_min": 150,
     "attack_max": 200,
-    "attack_min": 30,
+    "attack_min": 100,
     "defense_max": 200,
-    "defense_min": 30,
+    "defense_min": 100,
     "speed_max": 200,
     "speed_min": 100,
     "skillAttack_max": 150,
-    "skillAttack_min": 30,
+    "skillAttack_min": 80,
     "skillAccuracy_max": 200,
     "skillAccuracy_min": 50
 }
